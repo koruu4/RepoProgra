3cdac5f2f0b884ededcc21e46b5389107cfb438d
koruu4 <jaircast759@gmail.com>
2024-05-09 20:54:52 -0600

    .

--- ProyectoProgra.cpp
+++ ProyectoProgra.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
-#include <time.h>
-#include <conio.h>
+#include <time.h>  //eleccion random de primera ronda
+#include <conio.h> //system clear y gotoxy ;p
 #include <cstring> // Para memcpy
 using namespace std;
 string jugadorInicial, jugadorSecundario, jugadorAux;
@@ -11,16 +11,16 @@ string nombreJugador2;
 int turno = 0, contadorPartidas = 1;
 char tablero[3][3]{{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
 char valoresIniciales[3][3]{{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
-int fnMenu();
-int fnElegirJugadorInicial();
-int fnVerificarGanador();
-void fnIdentificarGanador(int turnoAnterior);
-void fnJugadorContraJugador();
-char fnPosiblesJugadas();
-void fnActualizarTurnos(int x, int y);
-int fnVerificarJugada(char jugada);
-// int fnJugadorContraComputadora();
-void construirTablero();
+int fnMenu();                                 // se muestra menu con 2 tipos de juego
+int fnElegirJugadorInicial();                 // se elige primer jugador solo en primera partida, se pide signo deseado
+int fnVerificarGanador();                     // Solo se verifica si alguien ganó la partida
+void fnIdentificarGanador(int turnoAnterior); // Se toma en cuenta en que turno se esta para obtener quien gano
+void fnJugadorContraJugador();                // toda la funcion de este modo de juego
+char fnPosiblesJugadas();                     // muestra jugadas posibles y disponibles
+void fnActualizarTurnos(int x, int y);        // despues de comprobar disponibilidad imprime turno
+int fnVerificarJugada(char jugada);           // verifica si la jugada enviada es posible o no
+void fnJugadorContraComputadora();            // version del juego solo contra la computadora
+void construirTablero();                      // construye tablero
 void construirTablero()
 {
     cout << "     0   1   2" << endl;
@@ -94,6 +94,68 @@ char fnPosiblesJugadas()
 
     cout << ", elige una jugada: ";
     return getch();
+}
+char fnPosiblesJugadasComputadora()
+{
+    cout << endl;
+    cout << "OPCIONES DEL JUEGO" << endl;
+    int contador = 0;
+    string opciones = "abcdefghi";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << opciones[contador] << " - [" << i << ", " << j << "]";
+            if (tablero[i][j] != ' ')
+            {
+                cout << " OCUPADO";
+            }
+            cout << endl;
+
+            contador++;
+        }
+    }
+    char vLetra;
+    int vNumeroJugada;
+    srand(time(0));
+    vNumeroJugada = 1 + rand() % (9 + 1 - 1);
+    if (vNumeroJugada == 1)
+    {
+        vLetra = 'a';
+    }
+    if (vNumeroJugada == 2)
+    {
+        vLetra = 'b';
+    }
+    if (vNumeroJugada == 3)
+    {
+        vLetra = 'c';
+    }
+    if (vNumeroJugada == 4)
+    {
+        vLetra = 'd';
+    }
+    if (vNumeroJugada == 5)
+    {
+        vLetra = 'e';
+    }
+    if (vNumeroJugada == 6)
+    {
+        vLetra = 'f';
+    }
+    if (vNumeroJugada == 7)
+    {
+        vLetra = 'g';
+    }
+    if (vNumeroJugada == 8)
+    {
+        vLetra = 'h';
+    }
+    if (vNumeroJugada == 9)
+    {
+        vLetra = 'i';
+    }
+    return vLetra;
 }
 int fnVerificarJugada(char jugada)
 {
@@ -229,13 +291,12 @@ void fnIdentificarGanador(int turnoAnterior)
     else
     {
         cout << "El ganador es : " << jugadorSecundario << "[" << signoTablero2 << "]" << endl;
-        jugadorAux=jugadorInicial;
-        jugadorInicial=jugadorSecundario;
-        jugadorSecundario=jugadorAux;
-        signoTableroaux=signoTablero1;
-        signoTablero1=signoTablero2;
-        signoTablero2=signoTablero1;
-        
+        jugadorAux = jugadorInicial;
+        jugadorInicial = jugadorSecundario;
+        jugadorSecundario = jugadorAux;
+        signoTableroaux = signoTablero1;
+        signoTablero1 = signoTablero2;
+        signoTablero2 = signoTablero1;
     }
 }
 void fnJugadorContraJugador()
@@ -274,10 +335,10 @@ void fnJugadorContraJugador()
         } while (signoTablero2 == signoTablero1);
     }
     if (contadorPartidas > 1)
-        {
-            memcpy(tablero, valoresIniciales, sizeof(valoresIniciales));
-            numeroDeTurnos = 0, banderaJugada = 0, banderaGanador = 0, turno = 0;
-        }
+    {
+        memcpy(tablero, valoresIniciales, sizeof(valoresIniciales));
+        numeroDeTurnos = 0, banderaJugada = 0, banderaGanador = 0, turno = 0;
+    }
     while (numeroDeTurnos < 9 && banderaGanador == 0)
     {
         system("cls");
@@ -301,6 +362,138 @@ void fnJugadorContraJugador()
         cout << "EL JUEGO TERMINO EN UN EMPATE..." << endl;
     }
 }
+void fnJugadorContraComputadora()
+{
+    int numeroDeTurnos = 0, banderaJugada = 0, banderaGanador = 0, vNumeroSigno;
+    if (contadorPartidas == 1)
+    {
+        int jugadorInicialEscoger;
+        nombreJugador2 = "Computadora :P";
+        cin.ignore();
+        cout << "Ingrese nombre de jugador = ";
+        getline(cin, nombreJugador1);
+        jugadorInicialEscoger = fnElegirJugadorInicial();
+        if (jugadorInicialEscoger == 1)
+        {
+            jugadorInicial = nombreJugador1;
+            jugadorSecundario = nombreJugador2;
+            cout << "El jugador que empezara jugando es " << jugadorInicial << endl;
+            cout << "Primer jugador, porfavor elija que signo usar o/x = ";
+            cin >> signoTablero1;
+            if (signoTablero1 == 'x')
+            {
+                signoTablero2 = 'o';
+            }
+            else
+            {
+                signoTablero2 = 'x';
+            }
+        }
+        else
+        {
+            jugadorInicial = nombreJugador2;
+            jugadorSecundario = nombreJugador1;
+            cout << "El jugador que empezara jugando es " << jugadorInicial << endl;
+            srand(time(0));
+            vNumeroSigno = 1 + rand() % (2 + 1 - 1);
+            if (vNumeroSigno == 1)
+            {
+                signoTablero1 = 'x';
+                signoTablero2 = 'o';
+            }
+            else
+            {
+                signoTablero1 = 'o';
+                signoTablero2 = 'x';
+            }
+            cout << "El jugador utilizara el signo " << signoTablero1<<endl;
+            cout << "La computadora utilizara el signo " << signoTablero2<<endl;
+            system("cls"); 
+        }
+    }
+    if (contadorPartidas > 1)
+    {
+        memcpy(tablero, valoresIniciales, sizeof(valoresIniciales));
+        numeroDeTurnos = 0, banderaJugada = 0, banderaGanador = 0, turno = 0;
+    }
+    while (numeroDeTurnos < 9 && banderaGanador == 0)
+    {
+        system("cls");
+        construirTablero();
+        if ((turno % 2 == 0 && jugadorInicial == nombreJugador2) || (turno % 2 != 0 && jugadorSecundario == nombreJugador2))
+        {
+            char vLetra;
+            int vNumeroJugada;
+            do
+            {
+                srand(time(0));
+                vNumeroJugada = 1 + rand() % (9 + 1 - 1);
+                if (vNumeroJugada == 1)
+                {
+                    vLetra = 'a';
+                }
+                if (vNumeroJugada == 2)
+                {
+                    vLetra = 'b';
+                }
+                if (vNumeroJugada == 3)
+                {
+                    vLetra = 'c';
+                }
+                if (vNumeroJugada == 4)
+                {
+                    vLetra = 'd';
+                }
+                if (vNumeroJugada == 5)
+                {
+                    vLetra = 'e';
+                }
+                if (vNumeroJugada == 6)
+                {
+                    vLetra = 'f';
+                }
+                if (vNumeroJugada == 7)
+                {
+                    vLetra = 'g';
+                }
+                if (vNumeroJugada == 8)
+                {
+                    vLetra = 'h';
+                }
+                if (vNumeroJugada == 9)
+                {
+                    vLetra = 'i';
+                }
+                banderaJugada = fnVerificarJugada(vLetra);
+            } while (banderaJugada == 0);
+            if (banderaJugada == 1)
+            {
+                banderaGanador = fnVerificarGanador();
+                numeroDeTurnos++;
+            }
+        }
+        else if ((turno % 2 == 0 && jugadorInicial == nombreJugador1) || (turno % 2 != 0 && jugadorSecundario == nombreJugador1))
+        {
+            banderaJugada = fnVerificarJugada(fnPosiblesJugadas());
+            if (banderaJugada == 1)
+            {
+                banderaGanador = fnVerificarGanador();
+                numeroDeTurnos++;
+            }
+        }
+        cout << endl;
+        system("cls");
+        construirTablero();
+        if (banderaGanador == 1)
+        {
+            fnIdentificarGanador(numeroDeTurnos - 1);
+        }
+        else
+        {
+            cout << "EL JUEGO TERMINO EN UN EMPATE..." << endl;
+        }
+    }
+}
 
 int main()
 {
@@ -319,7 +512,14 @@ int main()
         cout << "FIN DEL JUEGO :P" << endl;
         break;
     case 2:
-        // fnJugadorContraComputadora();
+        do{ 
+        fnJugadorContraComputadora();
+        cout << "Desea volver a jugar? s/n = ";
+            cin >> repetirPartida;
+            system("cls");
+            contadorPartidas++;
+        } while (repetirPartida == 's');
+        cout << "FIN DEL JUEGO :P" << endl;
         break;
     case 3:
         break;
